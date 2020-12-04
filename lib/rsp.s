5d16f873c5609652780f2a6660f44cd8c935c315
CrashOveride95 <crashoveride953@gmail.com>
2020-12-03 22:19:14 -0500

    Fix Shindou

--- lib/rsp.s
+++ lib/rsp.s
@@ -41,9 +41,18 @@ glabel gspFast3D_fifoTextEnd
 
 /* Audio Bins */
 
+/*
+ * "What the heck is this?"
+ * Shindou is the only version to actually edit the audio microcode, so I had to do this sorry lol
+ */
+
 .balign 16
 glabel aspMainTextStart
+.if VERSION_SH == 1
+    .incbin "rsp/audio.bin"
+.else
     .incbin "lib/PR/audio/aspMain.bin"
+.endif
 glabel aspMainTextEnd
 
 /*
@@ -186,7 +195,11 @@ glabel gspFast3D_fifoDataEnd
 
 .balign 16
 glabel aspMainDataStart
+.if VERSION_SH == 1
+    .incbin "rsp/audio_data.bin"
+.else
     .incbin "lib/PR/audio/aspMain_data.bin"
+.endif
 glabel aspMainDataEnd
 
 /* LESS COMMON MICROCODES */
