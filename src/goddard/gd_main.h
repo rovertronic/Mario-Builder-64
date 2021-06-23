3e73a6c09440d0ac332f3cf19d3bc437f576ec83
CrashOveride95 <crashoveride953@gmail.com>
2021-06-22 23:39:24 -0400

    fix overalignment

--- src/goddard/gd_main.h
+++ src/goddard/gd_main.h
@@ -10,14 +10,15 @@
 // "#define printf(...) /* nothing */" wasn't an option.)
 // This macro is separate from the gd_printf function; one probably
 // forwarded to the other, but it is hard to tell in which direction.
-#ifdef __GNUC__
+
+#if defined(ISVPRINT) || defined(UNF)
+#define printf osSyncPrintf
+#else
 #define printf(...)                                       \
     _Pragma ("GCC diagnostic push")                       \
     _Pragma ("GCC diagnostic ignored \"-Wunused-value\"") \
     (__VA_ARGS__);                                        \
     _Pragma ("GCC diagnostic pop")
-#else
-#define printf
 #endif
 
 // structs
