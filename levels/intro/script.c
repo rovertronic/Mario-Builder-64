689c70d25a3162ae80dbf48f554018fb59d89eb2
Reonu <danileon95@gmail.com>
2021-06-29 20:56:38 +0100

    skip goddard screen if goddard isn't built

--- levels/intro/script.c
+++ levels/intro/script.c
@@ -45,6 +45,9 @@ const LevelScript level_intro_splash_screen[] = {
 
 const LevelScript level_intro_mario_head_regular[] = {
     INIT_LEVEL(),
+#ifndef GODDARD
+    EXIT_AND_EXECUTE(/*seg*/ 0x14, _menuSegmentRomStart, _menuSegmentRomEnd, level_main_menu_entry_1),
+#endif
     BLACKOUT(/*active*/ TRUE),
     FIXED_LOAD(/*loadAddr*/ _goddardSegmentStart, /*romStart*/ _goddardSegmentRomStart, /*romEnd*/ _goddardSegmentRomEnd),
 #ifdef GODDARD
