820713290839ed7d14c4bd7ce45506a22f8c44db
Reonu <danileon95@gmail.com>
2021-08-05 00:11:17 +0100

    fixed super responsive controls define

--- src/game/mario_actions_moving.c
+++ src/game/mario_actions_moving.c
@@ -386,12 +386,8 @@ void update_shell_speed(struct MarioState *m) {
         m->forwardVel = 64.0f;
     }
 
-#ifdef SUPER_RESPONSIVE_CONTROLS
-    m->faceAngle[1] = m->intendedYaw;
-#else
     m->faceAngle[1] =
         m->intendedYaw - approach_s32((s16)(m->intendedYaw - m->faceAngle[1]), 0, 0x800, 0x800);
-#endif
 
 
     apply_slope_accel(m);
@@ -465,9 +461,13 @@ void update_walking_speed(struct MarioState *m) {
     if (m->forwardVel > 48.0f) {
         m->forwardVel = 48.0f;
     }
-
+    
+#ifdef SUPER_RESPONSIVE_CONTROLS
+    m->faceAngle[1] = m->intendedYaw;
+#else
     m->faceAngle[1] =
         m->intendedYaw - approach_s32((s16)(m->intendedYaw - m->faceAngle[1]), 0, 0x800, 0x800);
+#endif
     apply_slope_accel(m);
 }
 
