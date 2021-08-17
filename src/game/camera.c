ff9198975ac66d2ac38fa7f1eca4feaa6446e745
Reonu <danileon95@gmail.com>
2021-08-17 16:12:31 +0100

    force camera to 8 dir with CAMERA_FIX

--- src/game/camera.c
+++ src/game/camera.c
@@ -3509,6 +3509,9 @@ void init_camera(struct Camera *c) {
     gLakituState.nextYaw = gLakituState.yaw;
     c->yaw = gLakituState.yaw;
     c->nextYaw = gLakituState.yaw;
+    #ifdef CAMERA_FIX
+    set_camera_mode(c, CAMERA_MODE_8_DIRECTIONS, 0);
+    #endif
     #ifdef PUPPYCAM
     puppycam_init();
     #endif
