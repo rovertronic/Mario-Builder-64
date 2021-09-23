426d8ebae3b070c6cb72f13b66463edb3a32eca6
Arceveti <73617174+Arceveti@users.noreply.github.com>
2021-09-22 21:38:12 -0700

    Fix side flip ground pound visual bug

--- src/game/mario_actions_airborne.c
+++ src/game/mario_actions_airborne.c
@@ -603,10 +603,12 @@ s32 act_hold_freefall(struct MarioState *m) {
 
 s32 act_side_flip(struct MarioState *m) {
     if (m->input & INPUT_B_PRESSED) {
+        m->marioObj->header.gfx.angle[1] += 0x8000;
         return set_mario_action(m, ACT_DIVE, 0);
     }
 
     if (m->input & INPUT_Z_PRESSED) {
+        m->marioObj->header.gfx.angle[1] += 0x8000;
         return set_mario_action(m, ACT_GROUND_POUND, 0);
     }
 
