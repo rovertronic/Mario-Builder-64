13d7d2cafff217c4fa9d254ac28d545de11ce43f
Arceveti <73617174+Arceveti@users.noreply.github.com>
2021-09-26 17:13:33 -0700

    Whoops left debug level select on

--- src/boot/main.c
+++ src/boot/main.c
@@ -65,7 +65,7 @@ s8 sAudioEnabled = TRUE;
 u32 gNumVblanks = 0;
 s8 gResetTimer = 0;
 s8 gNmiResetBarsTimer = 0;
-s8 gDebugLevelSelect = TRUE;
+s8 gDebugLevelSelect = FALSE;
 
 s8 gShowProfiler = FALSE;
 s8 gShowDebugText = FALSE;
