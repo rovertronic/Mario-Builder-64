886d9841a8e0553027027ff05bef2ea2f5f4d8e9
Arceveti <arceveti@gmail.com>
2022-01-10 18:27:08 -0800

    Better Mr. I offset fix

--- src/game/behaviors/mr_i.inc.c
+++ src/game/behaviors/mr_i.inc.c
@@ -76,7 +76,7 @@ void bhv_mr_i_iris_loop(void) {
         obj_build_transform_from_pos_and_angle(o, O_PARENT_RELATIVE_POS_INDEX, O_MOVE_ANGLE_INDEX);
         obj_translate_local(o, O_POS_INDEX, O_PARENT_RELATIVE_POS_INDEX);
         o->oFaceAnglePitch = o->oMoveAnglePitch;
-        o->oGraphYOffset = o->parentObj->oBehParams2ndByte ? 200.0f : 100.0f;
+        o->oGraphYOffset = o->parentObj->oGraphYOffset;
     }
 
     if (!o->parentObj->oMrIBlinking) {
