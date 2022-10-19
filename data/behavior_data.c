fc6a6055d94d69882a021c963f0a5f4b4efe68d8
Gregory Heskett <gheskett@gmail.com>
2022-10-19 12:38:58 -0400

    Bugfix: WF stationary platforms given static collision despite being able to delete themselves (#510)
    Resulted in a memory leak and stale object referencing that impacted Mario's positioning

--- data/behavior_data.c
+++ data/behavior_data.c
@@ -1679,9 +1679,9 @@ const BehaviorScript bhvWfSolidTowerPlatform[] = {
     BEGIN(OBJ_LIST_SURFACE),
     OR_INT(oFlags, (OBJ_FLAG_SET_FACE_YAW_TO_MOVE_YAW | OBJ_FLAG_UPDATE_GFX_POS_AND_ANGLE)),
     LOAD_COLLISION_DATA(wf_seg7_collision_platform),
-    CALL_NATIVE(load_object_static_model),
     BEGIN_LOOP(),
         CALL_NATIVE(bhv_wf_solid_tower_platform_loop),
+        CALL_NATIVE(load_object_collision_model),
     END_LOOP(),
 };
 
