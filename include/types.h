a42bd9bc2ec8f4028c7f711a3ceb7c00f83fd371
Gregory Heskett <gheskett@gmail.com>
2022-11-03 03:52:19 -0400

    Implemented native Gamecube controller support and corresponding emulator hint in ROM header (#505)
    * Implemented native Gamecube controller support and corresponding emulator hint in ROM header
    
    * Added X and Y buttons to VALID_BUTTONS in sm64.h
    
    * Added workaround in controller type detection for mupen bug
    
    * read GC controllers from port 2
    
    * L to Z logic for GC controller uses a threshold
    
    * Changed threshold for L trig to around 33%
    
    Changed threshold based on feedback
    
    * Added gGamecubeControllerPort
    
    * fixed wrong check
    
    * Bugfix: Game crashes when less than 2 controllers are connected
    
    * Suppress compiler warnings
    
    * Bugfix: button swapping still takes place on non-GC controllers
    
    * Wrap port 2 stuff in console check
    
    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
    
    * GC console check
    
    * Funny emulator crash moment
    
    * block comment moment
    
    * Added centering for the gamecube controller, support reset command (#511)
    
    * Added centering for the gamecube controller, support reset command
    
    * Use struct 'ControllerCenters' for 'gGamecubeControllerCenters'
    
    * Store gamecube controller centers per controller
    
    * Clamp GC controller stick values to s8
    
    * the
    
    formatting changes
    
    Co-authored-by: Mr-Wiseguy <mrwiseguyromhacking@gmail.com>
    Co-authored-by: Reonu <danileon95@gmail.com>
    Co-authored-by: Denis Kopyrin <aglab3@gmail.com>

--- include/types.h
+++ include/types.h
@@ -42,7 +42,7 @@ struct Controller {
   /*0x12*/ u16 buttonPressed;
   /*0x14*/ u16 buttonReleased;
   /*0x18*/ OSContStatus *statusData;
-  /*0x1C*/ OSContPad *controllerData;
+  /*0x1C*/ OSContPadEx *controllerData;
 #if ENABLE_RUMBLE
   /*0x20*/ s32 port;
 #endif
