f80acbbefcf520f9fdc589b48da275535b16ed6f
mineqwerty <mineqwerty25@gmail.com>
2023-06-04 21:20:39 -0400

    testing changes

--- data/behavior_data.c
+++ data/behavior_data.c
@@ -4863,7 +4863,6 @@ const BehaviorScript bhvOrangeNumber[] = {
     BEGIN(OBJ_LIST_LEVEL),
     OR_INT(oFlags, OBJ_FLAG_UPDATE_GFX_POS_AND_ANGLE),
     BILLBOARD(),
-    SET_HOME(),
     CALL_NATIVE(bhv_orange_number_init),
     BEGIN_LOOP(),
         CALL_NATIVE(bhv_orange_number_loop),
