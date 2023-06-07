91af40e72ad45e6bbe688ac73d0b635b71414690
CowQuack <mineqwerty25@gmail.com>
2023-06-07 12:26:27 -0400

    Update src/game/behaviors/red_coin.inc.c
    Co-authored-by: thecozies <79979276+thecozies@users.noreply.github.com>

--- src/game/behaviors/red_coin.inc.c
+++ src/game/behaviors/red_coin.inc.c
@@ -51,6 +51,7 @@ void bhv_red_coin_loop(void) {
 
             // Spawn the orange number counter, as long as it isn't the last coin.
             if (o->parentObj->oHiddenStarTriggerCounter != o->parentObj->oHiddenStarTriggerTotal) {
+                // Cap visible count to 99
                 if (o->parentObj->oHiddenStarTriggerCounter > 99) {
                     spawn_orange_number(9, 28, 0, 0);
                     spawn_orange_number(9, -28, 0, 0);
