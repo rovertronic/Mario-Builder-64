fffebd106513bdc1db4c38482e9b33f2cc293b69
Reonu <danileon95@gmail.com>
2023-06-17 01:57:33 +0100

    add death on exit course define (#564)
    if the define is enabled, exiting course is the same as dying (warps to failure warp node)

--- src/game/level_update.c
+++ src/game/level_update.c
@@ -1069,9 +1069,16 @@ s32 play_mode_paused(void) {
         if (gDebugLevelSelect) {
             fade_into_special_warp(WARP_SPECIAL_LEVEL_SELECT, 1);
         } else {
+#ifdef DEATH_ON_EXIT_COURSE
+            raise_background_noise(1);
+            gCameraMovementFlags &= ~CAM_MOVE_PAUSE_SCREEN;
+            set_play_mode(PLAY_MODE_NORMAL);
+            level_trigger_warp(gMarioState, WARP_OP_DEATH);
+#else
             initiate_warp(EXIT_COURSE_LEVEL, EXIT_COURSE_AREA, EXIT_COURSE_NODE, WARP_FLAG_EXIT_COURSE);
             fade_into_special_warp(WARP_SPECIAL_NONE, 0);
             gSavedCourseNum = COURSE_NONE;
+#endif
         }
 
         gCameraMovementFlags &= ~CAM_MOVE_PAUSE_SCREEN;
