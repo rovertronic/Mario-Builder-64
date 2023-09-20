da3a7e826a4ffc4e9b6d8e633141f585636b7b3e
Gregory Heskett <gheskett@gmail.com>
2023-09-20 00:29:19 -0400

    Add missing JRB vanilla level check (#685)

--- src/game/behaviors/spawn_star.inc.c
+++ src/game/behaviors/spawn_star.inc.c
@@ -153,9 +153,13 @@ void spawn_no_exit_star(f32 x, f32 y, f32 z) {
 void bhv_hidden_red_coin_star_init(void) {
     struct Object *starObj = NULL;
 
+#ifdef ENABLE_VANILLA_LEVEL_SPECIFIC_CHECKS
     if (gCurrCourseNum != COURSE_JRB) {
         spawn_object(o, MODEL_TRANSPARENT_STAR, bhvRedCoinStarMarker);
     }
+#else
+    spawn_object(o, MODEL_TRANSPARENT_STAR, bhvRedCoinStarMarker);
+#endif
 
     // check if bparam2 specifies a total number of coins that should spawn the star
     if (o->oBehParams2ndByte != 0) {
