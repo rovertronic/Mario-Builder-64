e99de1012518493ef6f8247653f3fad074d42db5
someone2639 <someone2639@users.noreply.github.com>
2023-12-14 23:26:43 -0500

    Add relevant model ID asserts to level script load commands (#722)
    Co-authored-by: someone2639 <someone2639@gmail.com>

--- src/engine/level_script.c
+++ src/engine/level_script.c
@@ -9,6 +9,7 @@
 #include "buffers/framebuffers.h"
 #include "buffers/zbuffer.h"
 #include "game/area.h"
+#include "game/debug.h"
 #include "game/game_init.h"
 #include "game/mario.h"
 #include "game/memory.h"
@@ -462,6 +463,7 @@ static void level_cmd_load_model_from_dl(void) {
     s16 layer = CMD_GET(u16, 0x8);
     void *dl_ptr = CMD_GET(void *, 4);
 
+    assert(model < MODEL_ID_COUNT, "Tried to load an invalid model ID.");
     if (model < MODEL_ID_COUNT) {
         gLoadedGraphNodes[model] =
             (struct GraphNode *) init_graph_node_display_list(sLevelPool, 0, layer, dl_ptr);
@@ -474,6 +476,7 @@ static void level_cmd_load_model_from_geo(void) {
     ModelID16 model = CMD_GET(ModelID16, 2);
     void *geo = CMD_GET(void *, 4);
 
+    assert(model < MODEL_ID_COUNT, "Tried to load an invalid model ID.");
     if (model < MODEL_ID_COUNT) {
         gLoadedGraphNodes[model] = process_geo_layout(sLevelPool, geo);
     }
@@ -487,6 +490,7 @@ static void level_cmd_23(void) {
     void *dl  = CMD_GET(void *, 4);
     s32 scale = CMD_GET(s32, 8);
 
+    assert(model < MODEL_ID_COUNT, "Tried to load an invalid model ID.");
     if (model < MODEL_ID_COUNT) {
         // GraphNodeScale has a GraphNode at the top. This
         // is being stored to the array, so cast the pointer.
