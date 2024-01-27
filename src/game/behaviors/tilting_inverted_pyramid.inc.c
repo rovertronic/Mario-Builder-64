3380b88ffec56c03918a78eda1cce2cf7f63617a
RRovert <rovertentertainment@gmail.com>
2024-01-27 15:24:40 -0600

    fix badge not deleting your shell

--- src/game/behaviors/tilting_inverted_pyramid.inc.c
+++ src/game/behaviors/tilting_inverted_pyramid.inc.c
@@ -6096,6 +6096,7 @@ void bhv_badge(void) {
             }
             if (o->oDistanceToMario < 180.0f) {
                 o->oAction++;
+                mario_stop_riding_object(gMarioState);
                 save_file_set_badge_equip(1 << o->oBehParams2ndByte);
 
                 rtext_insert_pointer[0] = badgenames[o->oBehParams2ndByte];
