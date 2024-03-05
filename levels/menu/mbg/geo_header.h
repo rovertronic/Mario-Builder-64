5645c31b8c94e8be4c9fca28dfe655b8b4ec4ca2
RRovert <rovertentertainment@gmail.com>
2024-03-05 15:27:22 -0600

    get rid of black error on left of menu

--- levels/menu/mbg/geo_header.h
+++ levels/menu/mbg/geo_header.h
@@ -1,7 +1,7 @@
 extern const GeoLayout mbg_geo[];
-extern Vtx mbg_Plane_001_mesh_layer_1_vtx_0[804];
+extern Vtx mbg_Plane_001_mesh_layer_1_vtx_0[864];
 extern Gfx mbg_Plane_001_mesh_layer_1_tri_0[];
-extern Vtx mbg_Plane_001_mesh_layer_1_vtx_1[804];
+extern Vtx mbg_Plane_001_mesh_layer_1_vtx_1[864];
 extern Gfx mbg_Plane_001_mesh_layer_1_tri_1[];
 extern Gfx mat_mbg_makerstripe1[];
 extern Gfx mat_mbg_makerstripe2[];
