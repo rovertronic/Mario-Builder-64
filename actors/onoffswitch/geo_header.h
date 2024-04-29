67fee3b186c041bebbba398abfe2b3a4701cee50
RRovert <rovertentertainment@gmail.com>
2024-04-29 01:12:01 -0500

    switch bottom model

--- actors/onoffswitch/geo_header.h
+++ actors/onoffswitch/geo_header.h
@@ -6,7 +6,7 @@ extern u8 onoffswitch_redswitch_ci8[];
 extern u8 onoffswitch_redswitch_pal_rgba16[];
 extern u8 onoffswitch_blueswitch_ci8[];
 extern u8 onoffswitch_blueswitch_pal_rgba16[];
-extern Vtx onoffswitch_000_displaylist_mesh_layer_1_vtx_0[26];
+extern Vtx onoffswitch_000_displaylist_mesh_layer_1_vtx_0[40];
 extern Gfx onoffswitch_000_displaylist_mesh_layer_1_tri_0[];
 extern Gfx mat_onoffswitch_redswitch[];
 extern Gfx mat_revert_onoffswitch_redswitch[];
