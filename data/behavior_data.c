9cf0ad96538d52cdcc1df72b6b294ad61859f24b
RRovert <rovertentertainment@gmail.com>
2024-05-01 22:50:32 -0500

    flag drop

--- data/behavior_data.c
+++ data/behavior_data.c
@@ -6014,7 +6014,7 @@ const BehaviorScript bhvKoopa[] = {
 const BehaviorScript bhvKoopaRaceEndpoint[] = {
     BEGIN(OBJ_LIST_DEFAULT),
     OR_INT(oFlags, (OBJ_FLAG_COMPUTE_DIST_TO_MARIO | OBJ_FLAG_UPDATE_GFX_POS_AND_ANGLE)),
-    DROP_TO_FLOOR(),
+    //DROP_TO_FLOOR(),
     SPAWN_CHILD_WITH_PARAM(/*Bhv param*/ 0, /*Model*/ MODEL_KOOPA_FLAG, /*Behavior*/ bhvKoopaFlag),
     BEGIN_LOOP(),
         CALL_NATIVE(bhv_koopa_race_endpoint_update),
