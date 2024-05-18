afb173d85d23841b03d760bb5519fb254fae8613
RRovert <rovertentertainment@gmail.com>
2024-05-17 22:29:52 -0500

    turn off debug

--- include/config/config_debug.h
+++ include/config/config_debug.h
@@ -18,7 +18,7 @@
  * Enables a comprehensive standalone profiler. Automatically enabled by PUPPYPRINT_DEBUG.
  * If not using PUPPYPRINT_DEBUG, press L + D-Pad Up to toggle the profiler.
  */
-#define USE_PROFILER
+//#define USE_PROFILER
 
 /**
  * -- TEST LEVEL --
@@ -41,7 +41,7 @@
 /**
  * Enables a custom, enhanced performance profiler. (Enables PUPPYPRINT by default in config_safeguards).
  */
-#define PUPPYPRINT_DEBUG
+//#define PUPPYPRINT_DEBUG
 
 /**
  * Uses cycles instead of microseconds in Puppyprint debug output.
@@ -58,7 +58,7 @@
  * Visual debug enables some collision visuals. Tapping Right on the D-pad will cycle between visual hitboxes, visual surfaces, both, and neither.
  * If puppyprint is enabled, then this can be cycled only while the screen is active.
  */
-#define VISUAL_DEBUG
+//#define VISUAL_DEBUG
 
 /**
  * Opens all courses and doors. Used for debugging purposes to unlock all content.
