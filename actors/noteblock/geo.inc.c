d182411c724ede33c5b2d51115e3bd6e00325025
arthurtilly <arthurtilly413@gmail.com>
2024-05-22 08:56:41 +1200

    remove noteblock shadow

--- actors/noteblock/geo.inc.c
+++ actors/noteblock/geo.inc.c
@@ -1,7 +1,7 @@
 #include "src/game/envfx_snow.h"
 
 const GeoLayout noteblock_geo[] = {
-	GEO_SHADOW(SHADOW_SQUARE_PERMANENT, 0xB4,100),
+	GEO_NODE_START(),
 	GEO_OPEN_NODE(),
 		GEO_DISPLAY_LIST(1, noteblock_Cube_007_mesh_layer_1),
 		GEO_DISPLAY_LIST(1, noteblock_material_revert_render_settings),
