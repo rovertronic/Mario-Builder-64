c5edc19995705e5843cba9bdfb88ceb881c73cc6
RRovert <rovertentertainment@gmail.com>
2024-05-20 22:43:07 -0500

    texture blend file added

--- actors/maker/texscroll.inc.c
+++ actors/maker/texscroll.inc.c
@@ -1,85 +1,102 @@
-void scroll_sts_mat_maker_MakerLava() {
-	Gfx *mat = segmented_to_virtual(mat_maker_MakerLava);
-	shift_s(mat, 12, PACK_TILESIZE(0, 1));
-	shift_t(mat, 12, PACK_TILESIZE(0, 1));
-};
-
-void scroll_sts_mat_maker_MakerWater() {
-	Gfx *mat = segmented_to_virtual(mat_maker_MakerWater);
-	shift_s(mat, 12, PACK_TILESIZE(0, 1));
-	shift_t(mat, 12, PACK_TILESIZE(0, 1));
-};
-
-void scroll_sts_mat_maker_MakerRetroLava() {
-	static int intervalTex0 = 10;
-	static int curInterval0 = 10;
-	Gfx *mat = segmented_to_virtual(mat_maker_MakerRetroLava);
-
-	if (--curInterval0 <= 0) {
-		shift_s(mat, 12, PACK_TILESIZE(0, 5));
-		shift_t(mat, 12, PACK_TILESIZE(0, 5));
-		curInterval0 = intervalTex0;
-	}
-};
-
-void scroll_sts_mat_maker_MakerVoid() {
-	Gfx *mat = segmented_to_virtual(mat_maker_MakerVoid);
-	shift_s(mat, 12, PACK_TILESIZE(0, 1));
-	shift_t(mat, 12, PACK_TILESIZE(0, 1));
-	shift_t(mat, -1, PACK_TILESIZE(0, 1));
-};
-
-void scroll_sts_mat_maker_MakerQuicksand() {
-	Gfx *mat = segmented_to_virtual(mat_maker_MakerQuicksand);
-	shift_s(mat, 12, PACK_TILESIZE(0, 1));
-	shift_t(mat, 12, PACK_TILESIZE(0, 1));
-};
-
-void scroll_sts_mat_maker_MakerRetroWater() {
-	static int intervalTex0 = 10;
-	static int curInterval0 = 10;
-	Gfx *mat = segmented_to_virtual(mat_maker_MakerRetroWater);
-
-	if (--curInterval0 <= 0) {
-		shift_s(mat, 12, PACK_TILESIZE(0, 5));
-		shift_t(mat, 12, PACK_TILESIZE(0, 5));
-		curInterval0 = intervalTex0;
-	}
-};
-
-void scroll_sts_mat_maker_MakerServerAcid() {
+void scroll_gfx_mat_maker_MakerServerAcid() {
 	Gfx *mat = segmented_to_virtual(mat_maker_MakerServerAcid);
-	shift_s(mat, 12, PACK_TILESIZE(0, 1));
-	shift_t(mat, 12, PACK_TILESIZE(0, 1));
+
+	shift_s(mat, 15, PACK_TILESIZE(0, 1));
+	shift_t(mat, 15, PACK_TILESIZE(0, 1));
+
 };
 
-void scroll_sts_mat_maker_MakerGreenWater() {
+void scroll_gfx_mat_maker_MakerGreenWater() {
 	Gfx *mat = segmented_to_virtual(mat_maker_MakerGreenWater);
-	shift_s(mat, 12, PACK_TILESIZE(0, 1));
-	shift_t(mat, 12, PACK_TILESIZE(0, 1));
+
+	shift_s(mat, 16, PACK_TILESIZE(0, 1));
+	shift_t(mat, 16, PACK_TILESIZE(0, 1));
+
 };
 
-void scroll_sts_mat_maker_MakerBurningIce() {
+void scroll_gfx_mat_maker_MakerRetroLava() {
+	Gfx *mat = segmented_to_virtual(mat_maker_MakerRetroLava);
+	static int interval_tex_maker_MakerRetroLava = 10;
+	static int cur_interval_tex_maker_MakerRetroLava = 10;
+
+	if (--cur_interval_tex_maker_MakerRetroLava <= 0) {
+		shift_s(mat, 15, PACK_TILESIZE(0, 5));
+		shift_t(mat, 15, PACK_TILESIZE(0, 5));
+		cur_interval_tex_maker_MakerRetroLava = interval_tex_maker_MakerRetroLava;
+	}
+
+};
+
+void scroll_gfx_mat_maker_MakerVoid() {
+	Gfx *mat = segmented_to_virtual(mat_maker_MakerVoid);
+
+	shift_s(mat, 15, PACK_TILESIZE(0, 1));
+	shift_t(mat, 15, PACK_TILESIZE(0, 1));
+
+};
+
+void scroll_gfx_mat_maker_MakerQuicksand() {
+	Gfx *mat = segmented_to_virtual(mat_maker_MakerQuicksand);
+
+	shift_s(mat, 15, PACK_TILESIZE(0, 1));
+	shift_t(mat, 15, PACK_TILESIZE(0, 1));
+
+};
+
+void scroll_gfx_mat_maker_MakerRetroWater() {
+	Gfx *mat = segmented_to_virtual(mat_maker_MakerRetroWater);
+	static int interval_tex_maker_MakerRetroWater = 10;
+	static int cur_interval_tex_maker_MakerRetroWater = 10;
+
+	if (--cur_interval_tex_maker_MakerRetroWater <= 0) {
+		shift_s(mat, 16, PACK_TILESIZE(0, 5));
+		shift_t(mat, 16, PACK_TILESIZE(0, 5));
+		cur_interval_tex_maker_MakerRetroWater = interval_tex_maker_MakerRetroWater;
+	}
+
+};
+
+void scroll_gfx_mat_maker_MakerBurningIce() {
 	Gfx *mat = segmented_to_virtual(mat_maker_MakerBurningIce);
-	shift_s(mat, 12, PACK_TILESIZE(0, 1));
-	shift_t(mat, 12, PACK_TILESIZE(0, 1));
+
+	shift_s(mat, 15, PACK_TILESIZE(0, 1));
+	shift_t(mat, 15, PACK_TILESIZE(0, 1));
+
 };
 
-void scroll_sts_mat_maker_MakerVanillaLava() {
+void scroll_gfx_mat_maker_MakerWater() {
+	Gfx *mat = segmented_to_virtual(mat_maker_MakerWater);
+
+	shift_s(mat, 16, PACK_TILESIZE(0, 1));
+	shift_t(mat, 16, PACK_TILESIZE(0, 1));
+
+};
+
+void scroll_gfx_mat_maker_MakerLava() {
+	Gfx *mat = segmented_to_virtual(mat_maker_MakerLava);
+
+	shift_s(mat, 15, PACK_TILESIZE(0, 1));
+	shift_t(mat, 15, PACK_TILESIZE(0, 1));
+
+};
+
+void scroll_gfx_mat_maker_MakerVanillaLava() {
 	Gfx *mat = segmented_to_virtual(mat_maker_MakerVanillaLava);
-	shift_s(mat, 12, PACK_TILESIZE(0, 1));
-	shift_t(mat, 12, PACK_TILESIZE(0, 1));
+
+	shift_s(mat, 15, PACK_TILESIZE(0, 1));
+	shift_t(mat, 15, PACK_TILESIZE(0, 1));
+
 };
 
 void scroll_actor_dl_maker() {
-	scroll_sts_mat_maker_MakerLava();
-	scroll_sts_mat_maker_MakerWater();
-	scroll_sts_mat_maker_MakerRetroLava();
-	scroll_sts_mat_maker_MakerVoid();
-	scroll_sts_mat_maker_MakerQuicksand();
-	scroll_sts_mat_maker_MakerRetroWater();
-	scroll_sts_mat_maker_MakerServerAcid();
-	scroll_sts_mat_maker_MakerGreenWater();
-	scroll_sts_mat_maker_MakerBurningIce();
-	scroll_sts_mat_maker_MakerVanillaLava();
-}
+	scroll_gfx_mat_maker_MakerServerAcid();
+	scroll_gfx_mat_maker_MakerGreenWater();
+	scroll_gfx_mat_maker_MakerRetroLava();
+	scroll_gfx_mat_maker_MakerVoid();
+	scroll_gfx_mat_maker_MakerQuicksand();
+	scroll_gfx_mat_maker_MakerRetroWater();
+	scroll_gfx_mat_maker_MakerBurningIce();
+	scroll_gfx_mat_maker_MakerWater();
+	scroll_gfx_mat_maker_MakerLava();
+	scroll_gfx_mat_maker_MakerVanillaLava();
+};
