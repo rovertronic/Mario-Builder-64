2dd8f23ec989ff7707cca4d4d2f4262326f72a5f
arthurtilly <arthurtilly413@gmail.com>
2024-07-06 08:45:24 +1200

    fix stars in boxes bug

--- src/game/behaviors/breakable_box.inc.c
+++ src/game/behaviors/breakable_box.inc.c
@@ -108,8 +108,9 @@ void bhv_breakable_box_loop(void) {
     if (cur_obj_was_attacked_or_ground_pounded()) {
         obj_explode_and_spawn_coins(46.0f, COIN_TYPE_YELLOW);
         create_sound_spawner(SOUND_GENERAL_BREAK_BOX);
-        o->oPosY += 128;
-        cur_obj_drop_imbued_object(128);
+        o->oPosY += 150;
+        o->oHomeY += 150;
+        cur_obj_drop_imbued_object(0);
     }
 }
 
@@ -138,8 +139,9 @@ void bhv_breakable_box_rf_loop(void) {
     if (cur_obj_was_attacked_not_by_mario()) {
         obj_explode_and_spawn_coins(46.0f, COIN_TYPE_YELLOW);
         create_sound_spawner(SOUND_OBJ_POUNDING_LOUD);
-        o->oPosY += 128;
-        cur_obj_drop_imbued_object(128);
+        o->oPosY += 150;
+        o->oHomeY += 150;
+        cur_obj_drop_imbued_object(0);
     } else if (cur_obj_was_attacked_or_ground_pounded()) {
         if (o->oTimer > 15) {
             create_sound_spawner(SOUND_OBJ_BULLY_METAL);
