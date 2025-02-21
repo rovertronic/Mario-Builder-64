56470983e59f042f4fa9663668f3a289d5b87509
arthurtilly <arthurtilly413@gmail.com>
2025-02-21 18:55:53 +1300

    revert skeeters falling off edges

--- src/game/behaviors/skeeter.inc.c
+++ src/game/behaviors/skeeter.inc.c
@@ -159,7 +159,7 @@ void bhv_skeeter_update(void) {
     }
 
     obj_check_attacks(&sSkeeterHitbox, o->oAction);
-    cur_obj_move_standard(78);
+    cur_obj_move_standard(-78);
     cur_obj_die_if_on_death_barrier(MB64_STAR_HEIGHT);
 }
 
